55040245aae835e03758c81adc6e79630ec606ed
Abhay Singh Gill <abhaygill017@gmail.com>
2024-12-05 02:04:57 +0530

    common: Add lineage's qcom sepolicy_vndr os_pickup
    Change-Id: I643145ba6fd7933410410b33f7da2f0bb31b5a32
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- common.mk
+++ common.mk
@@ -9,32 +9,8 @@ ifeq ($(TARGET_BOARD_PLATFORM),)
 $(error "TARGET_BOARD_PLATFORM is not defined yet, please define in your device makefile so it's accessible to QCOM common.")
 endif
 
-# List of QCOM targets.
-MSMSTEPPE := sm6150
-TRINKET := trinket
-
-QCOM_BOARD_PLATFORMS += \
-    $(MSMSTEPPE) \
-    $(TRINKET) \
-    atoll \
-    bengal \
-    crow \
-    holi \
-    kona \
-    kalama \
-    lahaina \
-    lito \
-    monaco \
-    msm8937 \
-    msm8953 \
-    msm8996 \
-    msm8998 \
-    msmnile \
-    parrot \
-    sdm660 \
-    sdm710 \
-    sdm845 \
-    taro
+# Board defs
+include $(QCOM_COMMON_PATH)/defs.mk
 
 # List of targets that use video hardware.
 MSM_VIDC_TARGET_LIST ?= \
@@ -73,48 +49,6 @@ endif
 ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS),true)
 include vendor/qcom/opensource/core-utils/build/utils.mk
 endif
-
-6_1_FAMILY := \
-    pineapple
-
-# Kernel Families
-5_15_FAMILY := \
-    crow \
-    kalama \
-    monaco
-
-5_10_FAMILY := \
-    parrot \
-    taro
-
-5_4_FAMILY := \
-    holi \
-    lahaina
-
-4_19_FAMILY := \
-    bengal \
-    kona \
-    lito
-
-4_14_FAMILY := \
-    $(MSMSTEPPE) \
-    $(TRINKET) \
-    atoll \
-    msmnile
-
-4_9_FAMILY := \
-    msm8953 \
-    qcs605 \
-    sdm710 \
-    sdm845
-
-4_4_FAMILY := \
-    msm8998 \
-    sdm660
-
-3_18_FAMILY := \
-    msm8937 \
-    msm8996
 
 ifeq ($(call is-board-platform-in-list,$(6_1_FAMILY)),true)
 TARGET_KERNEL_VERSION ?= 6.1
